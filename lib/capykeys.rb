e7173ff872b633bec0bc201c4601a2f4c172bc56
Dakuan <dom.barker808@gmail.com>
2013-03-12 11:00:22 +0000

    nuanced event raising

--- lib/capykeys.rb
+++ lib/capykeys.rb
@@ -4,13 +4,13 @@ module Capykeys
   module KeyPress
 
     KEYS = {
-        left: 37,
-        right: 39,
-        enter: 13,
-        space: 32,
-        delete: 46,
-        backspace: 8,
-        A: 65
+      left: {keydown: 37, keypress: -1, keyup: 37},
+      right: {keydown: 39, keypress: -1, keyup: 39},
+      enter: {keydown: 13, keypress: 13, keyup: 13},
+      space: {keydown: 32, keypress: 32, keyup: 32},
+      delete: {keydown: 46, keypress: -1, keyup: 46},
+      backspace: {keydown: 8, keypress: -1, keyup: 8},
+      A: {keydown: 65, keypress: 65, keyup: 65}
     }
 
     def type_word
@@ -62,14 +62,14 @@ module Capykeys
     end
 
     private
-    
+
     def trigger_keypress(key)
       if page.evaluate_script("typeof jQuery === 'undefined'")
         raise 'Jquery needs to be in the page!'
       else
-        #page.driver.browser.execute_script("$(function(){var e = $.Event('keyup', { which: #{key} }); $(document).trigger(e);});")
-        page.driver.browser.execute_script("$(function(){var e = $.Event('keypress', { which: #{key} }); $(document).trigger(e);});")
-        #page.driver.browser.execute_script("$(function(){var e = $.Event('keydown', { which: #{key} }); $(document).trigger(e);});")
+        page.driver.browser.execute_script("$(function(){var e = $.Event('keydown', { which: #{key[:keydown]} }); $(document).trigger(e);});")        
+        page.driver.browser.execute_script("$(function(){var e = $.Event('keypress', { which: #{key[:keypress]} }); $(document).trigger(e);});")
+        page.driver.browser.execute_script("$(function(){var e = $.Event('keyup', { which: #{key[:keyup]} }); $(document).trigger(e);});")
       end
     end
   end
